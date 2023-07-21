d740fad16df3b6cd8834d114aebe7f76361569dd
Ryan Lee <ryanslee91@gmail.com>
2023-07-21 16:19:14 -0400

    commit 2

--- types/index.ts
+++ types/index.ts
@@ -5,4 +5,32 @@ export interface CustomButtonProps {
     containerStyles?: string;
     btnType?: "button" | "submit"
     handleClick?: MouseEventHandler<HTMLButtonElement>;
+    textStyles?: string;
+    rightIcon?: string;
+    isDisabled?: boolean;
+}
+
+export interface SearchManufacturerProps {
+    manufacturer: string;
+    // setManufacturer is a function doesn't return anything, its just a state, thats why there is void 
+    setManufacturer: (manufacturer: string) => void;
+}
+
+export interface HomeProps {
+    
+}
+
+export interface CarProps {
+    city_mpg: number;
+    class: string;
+    combination_mpg: number;
+    cylinders: number;
+    displacement: number;
+    drive: string;
+    fuel_type: string;
+    highway_mpg: number;
+    make: string;
+    model: string;
+    transmission: string;
+    year: number;
 }
